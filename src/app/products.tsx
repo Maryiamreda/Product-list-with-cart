b079e473e02f3ed0c49526c6d83f6f029750e84c
Maryiamreda <maryiamreda@gmail.com>
2025-06-22 00:12:21 +0300

    grid view problem

--- src/app/products.tsx
+++ src/app/products.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Products } from "@/data";
+import Image from "next/image";
 
 const ProductsList = () => {
   return (
-     <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
-         {Products.map((product, index) => (
-           <div key={index} className="card">
-             <img
-               src={product.image.thumbnail} 
+     <div className="">
+        <h1>  Desserts</h1>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 '>
+           {Products.map((product, index) => (
+           <div key={index} className="flex flex-col w-100">
+                  <Image
+               src={product.image.desktop} 
                alt={product.name}
-               className="w-full"
-             />
+             width={100}
+             height={100}
+               className=""
+/>          
              <h2>{product.name}</h2>
              <p>{product.category}</p>
              <p>${product.price.toFixed(2)}</p>
            </div>
          ))}
+        </div>
+        
        </div>
   );
 }
