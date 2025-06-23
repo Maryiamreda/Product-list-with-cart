67ca1632e4f9939ae58c5280bd13c43e1b41f899
Maryiamreda <maryiamreda@gmail.com>
2025-06-23 18:04:00 +0300

    order total

--- src/app/YourCart.tsx
+++ src/app/YourCart.tsx
@@ -7,7 +7,7 @@ const YourCart = () => {
      if (!context) {
         throw new Error('useCart must be used within a CartProvider');
       }
-        const { cart } = context;
+        const { cart , removeFromCart } = context;
     
   return (
     <div className='bg-white rounded-xl h-fit w-76 px-3 pt-3 pb-11 flex flex-col gap-5'>
@@ -20,23 +20,38 @@ const YourCart = () => {
                        height={95}
                        className="border rounded "
                     /> 
-                              <p className='text-Rose-500 text-xs font-semibold'>  Your added items will appear here</p>
-     </div>: 
+                    <p className='text-Rose-500 text-xs font-semibold'>  Your added items will appear here</p>
+     </div>
+     : 
      <div className='flex flex-col gap-5'>
-
         {cart.map((product, index) => (
-                   <div key={index} className="border-b-[0.08px] border-b-light-grayish-blue">
-                            <h2 className='text-xs font-semibold  text-start'>{product.name}</h2>
+                   <div key={index} className="border-b-[0.08px] border-b-light-grayish-blue flex justify-between">
+                    <div>
+                          <h2 className='text-xs font-semibold  text-start'>{product.name}</h2>
                             <div className='flex gap-2 pt-2 '>
                             <p className='font-semibold text-sm text-Red'>{product.amount}x</p>
                             <p className='  text-Rose-400 text-sm '><span className='text-xs'>@</span> ${product.price.toFixed(2)}</p>
                             <p className='text-Rose-500 font-semibold'>${product.amount * product.price}</p>
 
                             </div>
-                   
-                   
+                    </div>
+                    <div className='border-1 cursor-pointer  border-Rose-300 hover:border-black rounded-full w-4.5 h-4.5 flex justify-center items-center  '
+                    onClick={()=>removeFromCart(product)}
+                    >
+                       <Image
+                       src="/assets/images/icon-remove-item.svg"
+                       alt='icon-add-to-cart'
+                        width={10}
+                       height={10}
+                       className="icon-hover-black"
+                    /> 
+                    </div>
                    </div>
                  ))}
+      <div className='flex justify-between items-center'>
+      <p className='text-Rose-900 text-sm'>Order Total</p>
+      <h1 className='font-bold text-2xl '>${cart.reduce((accumulator, item) => accumulator + item.price * item.amount, 0)}</h1>
+    </div>
         </div>
         }
    
